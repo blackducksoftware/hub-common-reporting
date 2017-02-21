ac76ba4b45b715d77911aa75a62ce50905a92446
jrichard <jrichard@blackducksoftware.com>
2017-02-21 11:03:10 -0500

    Adding initial files

---
+++ src/main/java/com/blackducksoftware/integration/hub/report/RiskReportWriter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.hub.report;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import com.blackducksoftware.integration.hub.report.exception.RiskReportException;
+import com.google.gson.Gson;
+
+public class RiskReportWriter {
+
+    private final Gson gson = new Gson();
+
+    public void createHtmlReportFiles(final File outputDirectory, final ReportData reportData) throws RiskReportException {
+        try {
+            final RiskReportResourceCopier copier = new RiskReportResourceCopier(outputDirectory.getCanonicalPath());
+            File htmlFile = null;
+            try {
+                final List<File> writtenFiles = copier.copy();
+                for (final File file : writtenFiles) {
+                    if (file.getName().equals(RiskReportResourceCopier.RISK_REPORT_HTML_FILE_NAME)) {
+                        htmlFile = file;
+                        break;
+                    }
+                }
+            } catch (final URISyntaxException e) {
+                throw new RiskReportException("Couldn't create the report: " + e.getMessage(), e);
+            }
+            if (htmlFile == null) {
+                throw new RiskReportException("Could not find the file : " + RiskReportResourceCopier.RISK_REPORT_HTML_FILE_NAME
+                        + ", the report files must not have been copied into the report directory.");
+            }
+            String htmlFileString = FileUtils.readFileToString(htmlFile, "UTF-8");
+            final String reportString = gson.toJson(reportData);
+            htmlFileString = htmlFileString.replace(RiskReportResourceCopier.JSON_TOKEN_TO_REPLACE, reportString);
+            FileUtils.writeStringToFile(htmlFile, htmlFileString, "UTF-8");
+        } catch (final IOException e) {
+            throw new RiskReportException("Couldn't create the report: " + e.getMessage(), e);
+        }
+    }
+
+}
