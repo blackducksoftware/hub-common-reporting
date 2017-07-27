ac9a2fcb08a30f4ba56ff87939f986181365864a
jrichard <jrichard@blackducksoftware.com>
2017-07-27 18:10:38 -0400

    Fixing join method

--- src/main/java/com/blackducksoftware/integration/hub/report/pdf/StringWrapper.java
+++ src/main/java/com/blackducksoftware/integration/hub/report/pdf/StringWrapper.java
@@ -30,52 +30,52 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 public class StringWrapper {
-	
-	public static final int DEFAULT_CHAR_LENGTH = 20;
-	
-	public static String wrap(String str, int charLimit) {
-		ArrayList<String> subWords = new ArrayList<String>(Arrays.asList(str.split(" ")));		
-		for (int i = 0; i < subWords.size(); i++) {
-			String currentSubWord = subWords.get(i);
-			if (subWords.get(i).length() > charLimit) {
-				subWords.remove(currentSubWord);
-				subWords.addAll(i, modifyWrapString(currentSubWord, charLimit));
-			}
-		}
-		return String.join(" ", subWords);
-	}
-	
-	public static String wrap(String str) {
-		ArrayList<String> subWords = new ArrayList<String>(Arrays.asList(str.split(" ")));		
-		for (int i = 0; i < subWords.size(); i++) {
-			String currentWord = subWords.get(i);
-			if (currentWord.length() > DEFAULT_CHAR_LENGTH) {
-				subWords.remove(currentWord);
-				subWords.addAll(i, modifyWrapString(currentWord, DEFAULT_CHAR_LENGTH));
-			}
-		}
-		return String.join(" ", subWords);
-	}
-	
-	public static List<String> modifyWrapString(String str, int charLimit) {
-		int lastBreak = 0;
-		int maxLengthCounter = 0;
-		int strLen = str.length();
-		ArrayList<String> brokenUpStrings = new ArrayList<String>();
-		for (int i = 0; i < strLen; i++) {
-			if (!StringUtils.isAlphanumeric(str.charAt(i)+"") || maxLengthCounter == charLimit) {
-				brokenUpStrings.add(str.substring(lastBreak, i) );
-				lastBreak = i;
-				maxLengthCounter = 0;
-			} else {
-				maxLengthCounter++;
-			}
 
-			if (brokenUpStrings.size() > 0 && strLen - i < charLimit) {
-				brokenUpStrings.add(str.substring(i, strLen));
-				break;
-			}
-		}
-		return brokenUpStrings;
-	}
+    public static final int DEFAULT_CHAR_LENGTH = 20;
+
+    public static String wrap(final String str, final int charLimit) {
+        final ArrayList<String> subWords = new ArrayList<>(Arrays.asList(str.split(" ")));
+        for (int i = 0; i < subWords.size(); i++) {
+            final String currentSubWord = subWords.get(i);
+            if (subWords.get(i).length() > charLimit) {
+                subWords.remove(currentSubWord);
+                subWords.addAll(i, modifyWrapString(currentSubWord, charLimit));
+            }
+        }
+        return StringUtils.join(" ", subWords);
+    }
+
+    public static String wrap(final String str) {
+        final ArrayList<String> subWords = new ArrayList<>(Arrays.asList(str.split(" ")));
+        for (int i = 0; i < subWords.size(); i++) {
+            final String currentWord = subWords.get(i);
+            if (currentWord.length() > DEFAULT_CHAR_LENGTH) {
+                subWords.remove(currentWord);
+                subWords.addAll(i, modifyWrapString(currentWord, DEFAULT_CHAR_LENGTH));
+            }
+        }
+        return StringUtils.join(" ", subWords);
+    }
+
+    public static List<String> modifyWrapString(final String str, final int charLimit) {
+        int lastBreak = 0;
+        int maxLengthCounter = 0;
+        final int strLen = str.length();
+        final ArrayList<String> brokenUpStrings = new ArrayList<>();
+        for (int i = 0; i < strLen; i++) {
+            if (!StringUtils.isAlphanumeric(str.charAt(i) + "") || maxLengthCounter == charLimit) {
+                brokenUpStrings.add(str.substring(lastBreak, i));
+                lastBreak = i;
+                maxLengthCounter = 0;
+            } else {
+                maxLengthCounter++;
+            }
+
+            if (brokenUpStrings.size() > 0 && strLen - i < charLimit) {
+                brokenUpStrings.add(str.substring(i, strLen));
+                break;
+            }
+        }
+        return brokenUpStrings;
+    }
 }
